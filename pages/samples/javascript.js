0b350f3bf3dab4473039d3c88dba365c6a8759f9
rnkr1998 <naveenreddy551@gmail.com>
2020-03-02 20:54:12 +0530

    s

---
+++ pages/samples/javascript.js
@@ -0,0 +1,22 @@
+
+
+
+
+    $(document).ready(function(){
+     $(".available").click(function()
+     {
+       $(this).css("filter","opacity(50%)");
+       $(this).css("background","red");
+      
+     
+      
+      
+     })
+    });
+    
+    function myfun(s)
+      {
+        alert(" Your selected seat is : " +s);
+      }
+    
+    
